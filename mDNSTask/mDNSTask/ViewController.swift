e96b72f6f5a4539697cf8d53aecd1fe4a6d4719c
TirumalaiVenkkat <117457361+TirumalaiVenkkat@users.noreply.github.com>
2022-11-04 19:10:14 +0530

    Empty project created -- Venkkat

---
+++ mDNSTask/mDNSTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mDNSTask
+//
+//  Created by trioangle on 04/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
